5c1b6219d7c401ca83a5514da669c6c27fd7b392
Cheng Peng <peng.cheng@unibz.it>
2020-03-30 00:36:20 +0800

    First Commit

---
+++ src/main/java/Test_Choco.java
@@ -0,0 +1,8 @@
+import org.chocosolver.solver.Model;
+
+public class Test_Choco {
+	public static void main(String[] args) {
+	    Model model = new Model("A first model");
+	    System.out.println(model.getName());
+	}
+}
