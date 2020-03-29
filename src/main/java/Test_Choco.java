609064aab75a12a5ab3771e9f5df31894f320b05
Cheng Peng <peng.cheng@unibz.it>
2020-03-30 00:49:40 +0800

    Modify Test_Choco.java

--- src/main/java/Test_Choco.java
+++ src/main/java/Test_Choco.java
@@ -4,5 +4,6 @@ public class Test_Choco {
 	public static void main(String[] args) {
 	    Model model = new Model("A first model");
 	    System.out.println(model.getName());
+	    System.out.println("Happy Choco");
 	}
 }
